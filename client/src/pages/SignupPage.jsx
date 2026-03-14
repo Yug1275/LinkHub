1ec97e47a1a7bff7c9aff84b889fbbcd51aa6978
Yug1275 <yjpatel1275@gmail.com>
2026-03-14 11:24:52 +0530

    Initial commit - LinkHub project setup

---
+++ client/src/pages/SignupPage.jsx
@@ -0,0 +1,9 @@
+const SignupPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Signup</h1>
+    </div>
+  );
+};
+
+export default SignupPage;
